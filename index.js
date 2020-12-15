a316cc97b9912ece2a1cbc1550390cbcdf807cb1
Charbel t <charbel2000.52@gmail.com>
2020-12-15 05:43:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ class Formatter {
   static capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  static sanitize(sentence){
+  static sanitize(string){
     return string.replace( /[^A-Za-z0-9 '-]/g, '' );
   }
-  static titleize(string){
+  static titleize(sentence){
     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
     let arrayOfWords = sentence.split( " " )
